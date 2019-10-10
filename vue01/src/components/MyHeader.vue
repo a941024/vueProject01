353bffe3be17e0b8551cc91ede261a1b0a20c16e
Ameng <251256805@qq.com>
2019-10-10 15:59:35 +0800

    Ameng update his code.

--- vue01/src/components/MyHeader.vue
+++ vue01/src/components/MyHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="header">
     <router-link :to="{path:'/home'}">主页</router-link>
-    <router-link :to="{}">页面一</router-link>
+    <router-link :to="{path: '/pageOne'}">页面一</router-link>
     <router-link :to="{}">页面二</router-link>
   </div>
 
--- vue01/src/router/index.js
+++ vue01/src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from  '../components/wjh/home'
+import pageOne from '../components/sjx/Pageone'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path:'/home',redirect:'/'},
-    {path:'/',component:home}
+    {path:'/',component:home},
+    {path:'/pageOne',component: pageOne}
   ]
 })
