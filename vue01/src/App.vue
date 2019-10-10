5a44f899769d30daa5aa697774edb3f97709a091
LAPTOP-HV8HMNJQ\hp <2432448139@qq.com>
2019-10-10 19:13:49 +0800

    开始文件

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div id="app">
-    <MyHeader></MyHeader>
     <router-view/>
-    <MyFotter></MyFotter>
   </div>
 </template>
 
 <script>
 
-import MyHeader from "./components/MyHeader";
-import MyFotter from "./components/MyFotter";
 export default {
   name: 'App',
-  components: {MyFotter, MyHeader}
+  components: {}
 }
 </script>
 
 <style>
-  *{
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-    list-style: none;
-  }
-  html,body,#app{
-    width: 100%;
-    height: 100%;
-  }
+
 </style>
