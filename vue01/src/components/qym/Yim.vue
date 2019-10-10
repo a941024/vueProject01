d2f610a984738a1f6b9ce65430aaa41dca9c3045
DESKTOP-5TBM399\lx <941111024@qq.com>
2019-10-10 16:06:33 +0800

    qym new vue

--- vue01/src/components/qym/Yim.vue
+++ vue01/src/components/qym/Yim.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <fieldset>
+      <legend><h2>你们好慢啊</h2></legend>
+      <ul>
+        <li>1楼: 真的慢!</li>
+        <li>2楼: 等了好久!</li>
+        <li>3楼: 都快睡着了</li>
+      </ul>
+    </fieldset>
 </template>
 
 <script>
